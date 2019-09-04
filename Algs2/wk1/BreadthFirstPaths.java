a2e092ad6a936494f94c1c013aa6c95f1e42e011
Jacob Stewart <jacobstewart1987@gmail.com>
2019-09-04 08:06:00 -0700

    BFS

---
+++ Algs2/wk1/BreadthFirstPaths.java
@@ -0,0 +1,29 @@
+public class BreadthFirstPaths
+{
+    private boolean[]   marked;
+    private int[]       edgeTo;
+    private int[]       distTo;
+
+
+    private void bfs(Graph G, int s)
+    {
+        Queue<Integer> q = new Queue<Integer>();
+        q.enqueue(s);
+        marked[s] = true;
+        distTo[s] = 0;
+        while (!q.isEmpty())
+        {
+            int v = q.dequeue();
+            for (int w : G.adj(v))
+            {
+                if (!marked[w])
+                {
+                    q.enqueue(w);
+                    marked[w] = true;
+                    edgeTo[w] = v;
+                    distTo[w] = distTo[v] + 1;
+                }
+            }
+        }
+    }
+}
