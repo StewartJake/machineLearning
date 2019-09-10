970946db4139219f6b97cc7cffbb3552534a20ba
Jacob Stewart <jacobstewart1987@gmail.com>
2019-09-10 08:12:06 -0700

    WordNet API Outline

---
+++ Algs2/wk1/WordNet/WordNet.java
@@ -0,0 +1,20 @@
+public class WordNet
+{
+    public Wordnet(String synets, String hypernyms)
+    {}
+
+    public Iterable<String> nouns()
+    {}
+
+    public boolean isNoun(String word)
+    {}
+
+    public int distance(String nounA, String nounB)
+    {}
+
+    public String sap(Sting nounA, String nounB)
+    {}
+
+    public static void main(String[] args)
+    {}
+}
