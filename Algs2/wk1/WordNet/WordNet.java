bb330858bf3007e9821e4f6a65a25d8ab1d60618
Jacob Stewart <jacobstewart1987@gmail.com>
2019-09-12 08:12:59 -0700

    Started Wordnet

--- Algs2/wk1/WordNet/WordNet.java
+++ Algs2/wk1/WordNet/WordNet.java
@@ -1,19 +1,37 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import edu.princeton.cs.algs4.In;
+
 public class WordNet
 {
-    public Wordnet(String synets, String hypernyms)
-    {}
+    private Queue<String> nouns = new LinkedList<String>();
 
-    public Iterable<String> nouns()
-    {}
 
-    public boolean isNoun(String word)
-    {}
+    public WordNet(String synets, String hypernyms)
+    {
+        In synF = new In(synets);
+        while (synF.hasNextLine())
+        {
+            String      line        = synF.readLine();
+            String[]    fields      = line.split(",");
+            String[]    words       = fields[1].split(" ");
+            for (int i = 0; i < words.length; i++)
+                if (!nouns.contains(words[i]))
+                    nouns.add(words[i]);
+        }
+    }
 
-    public int distance(String nounA, String nounB)
-    {}
+    // public Iterable<String> nouns()
+    // {}
 
-    public String sap(Sting nounA, String nounB)
-    {}
+    // public boolean isNoun(String word)
+    // {}
+
+    // public int distance(String nounA, String nounB)
+    // {}
+
+    // public String sap(Sting nounA, String nounB)
+    // {}
 
     public static void main(String[] args)
     {}
