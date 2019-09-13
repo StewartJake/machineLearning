6f6ae3cb761221e6d34cce824f41140a49231593
Jacob Stewart <jacobstewart1987@gmail.com>
2019-09-13 08:05:26 -0700

    working on WordNet

--- Algs2/wk1/WordNet/WordNet.java
+++ Algs2/wk1/WordNet/WordNet.java
@@ -1,11 +1,11 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.HashMap;
+import java.util.Map;
 import edu.princeton.cs.algs4.In;
 
 public class WordNet
 {
-    private Queue<String> nouns = new LinkedList<String>();
-
+    private Map<int, String>   nouns   = new HashMap<>();
+    private SAP             wn;
 
     public WordNet(String synets, String hypernyms)
     {
@@ -16,16 +16,17 @@ public class WordNet
             String[]    fields      = line.split(",");
             String[]    words       = fields[1].split(" ");
             for (int i = 0; i < words.length; i++)
-                if (!nouns.contains(words[i]))
-                    nouns.add(words[i]);
+                nouns.put(words[i]);
         }
+        Digraph G = new Digraph(nouns.length);
+
     }
 
-    // public Iterable<String> nouns()
-    // {}
+    public Iterable<String> nouns()
+    {   return nouns;   }
 
-    // public boolean isNoun(String word)
-    // {}
+    public boolean isNoun(String word)
+    {   return nouns.contains(word);    }
 
     // public int distance(String nounA, String nounB)
     // {}
