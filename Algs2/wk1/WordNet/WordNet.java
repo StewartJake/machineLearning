3a3045ef978859ccddf49c17596040a770a83b72
Jacob Stewart <jacobstewart1987@gmail.com>
2019-09-15 17:06:05 -0700

    Worked on underlying structs

--- Algs2/wk1/WordNet/WordNet.java
+++ Algs2/wk1/WordNet/WordNet.java
@@ -1,32 +1,55 @@
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
 
 public class WordNet
 {
-    private Map<int, String>   nouns   = new HashMap<>();
-    private SAP             wn;
+    private Map<Integer, Bag<String>>   nouns   = new HashMap<>();
+    private Queue<String>               single  = new LinkedList<>();
+    private SAP                         wn;
 
     public WordNet(String synets, String hypernyms)
     {
         In synF = new In(synets);
         while (synF.hasNextLine())
         {
-            String      line        = synF.readLine();
-            String[]    fields      = line.split(",");
-            String[]    words       = fields[1].split(" ");
+            String      line    = synF.readLine();
+            String[]    fields  = line.split(",");
+            String[]    words   = fields[1].split(" ");
+            Bag<String> bag     = new Bag<>();
             for (int i = 0; i < words.length; i++)
-                nouns.put(words[i]);
+            {
+                bag.add(words[i]);
+                single.add(words[i]);
+            }
+            nouns.put(Integer.parseInt(fields[0]), bag);
         }
-        Digraph G = new Digraph(nouns.length);
+        Digraph G = new Digraph(nouns.size());
 
+        In hypF = new In(hypernyms);
+        while (hypF.hasNextLine())
+        {
+            String      line    =   hypF.readLine();
+            String[]    fields  =   line.split(",");
+            int         v       =   Integer.parseInt(fields[0]);
+            int         w;
+            for (int i = 1; i < fields.length; i++)
+            {
+                w = Integer.parseInt(fields[i]);
+                G.addEdge(v, w);
+            }
+        }
     }
 
     public Iterable<String> nouns()
-    {   return nouns.values();   }
+    {   return single;   }
 
     public boolean isNoun(String word)
-    {   return nouns.containsValue(word);    }
+    {   return single.contains(word);    }
 
     // public int distance(String nounA, String nounB)
     // {}
