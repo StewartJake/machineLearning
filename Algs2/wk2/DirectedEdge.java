c5534236097a70cc8ef0fa5c55261bc9e3ca46d2
Jacob Stewart <jacobstewart1987@gmail.com>
2019-10-06 17:57:44 -0700

    filled out fns

--- Algs2/wk2/DirectedEdge.java
+++ Algs2/wk2/DirectedEdge.java
@@ -1,8 +1,21 @@
 public class DirectedEdge
 {
-    DirectedEdge(int v, int w, double weight)
-    int from()
-    int to()
-    double wieght()
-    String toString()
+    private final int v, w;
+    private final double weight;
+
+    public DirectedEdge(int v, int w, double weight)
+    {
+        this.v = v;
+        this.w = w;
+        this.weight = weight;
+    }
+
+    public int from()
+    {   return v;   }
+
+    public int to()
+    {   return w;   }
+
+    public double weight()
+    {   return weight;  }
 }
