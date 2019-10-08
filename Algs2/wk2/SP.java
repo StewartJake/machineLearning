9d6f25ab34c564dafe725b48b453d25f0fb033c0
Jacob Stewart <jacobstewart1987@gmail.com>
2019-10-07 19:33:13 -0700

    unit tester

--- Algs2/wk2/SP.java
+++ Algs2/wk2/SP.java
@@ -3,4 +3,14 @@ public class SP
     public SP(EdgeWeightedDigraph g, int s)
     public double distTo(int v)
     public Iterable <DirectedEdge> pathTo(int v)
+    public static void main(String[] args)
+    {
+        SP sp = new SP(G, s);
+        for (int i = 0; i < G.V(); v++)
+        {
+            StdOut.printf("%d to %d (%.2f): ", s, i, sp.distTo(i));
+            for (DirectedEdge e : sp.pathTo(i))
+                StdOut.print(e + " ");
+            StdOut.println();
+        }
 }
