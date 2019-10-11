a160f3edff00ee8082c46303d39599acb8d050d9
Jacob Stewart <jacobstewart1987@gmail.com>
2019-10-10 21:15:26 -0700

    Starting a Dijkstra shortest path

---
+++ Algs2/wk2/DijkstraSP.java
@@ -0,0 +1,12 @@
+public class DijkstraSP
+{
+    private DirectedEdge[] edgeTo;
+    private double[] distTo;
+    private IndexMinPQ<Double> pq;
+
+    public DijkstraSP(EdgeWeightedDigrapgh G, int s)
+    {}
+
+    private void relax(DirectedEdge e)
+    {}
+}
