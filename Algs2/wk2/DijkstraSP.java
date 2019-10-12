379fb5e3c3690acafecc05cc0b29b4d9c888970d
Jacob Stewart <jacobstewart1987@gmail.com>
2019-10-11 22:19:15 -0700

    Added initializer

--- Algs2/wk2/DijkstraSP.java
+++ Algs2/wk2/DijkstraSP.java
@@ -5,7 +5,23 @@ public class DijkstraSP
     private IndexMinPQ<Double> pq;
 
     public DijkstraSP(EdgeWeightedDigrapgh G, int s)
-    {}
+    {
+        edgeTo = new DirectedEdge[G.V()];
+        distTo = new double[G.V()];
+        pq = new IndexMinPQ<Double>(G.V());
+
+        for (int v = 0; v < G.V(); v++)
+            distTo[v] = Double.POSTIVIE_INFINITY;
+        distTo[s] = 0.0;
+
+        pq.insert(s, 0.0);
+        while (!pq.isEmpty())
+        {
+            int v = pq.delMin();
+            for (DirectedEdge e : G.adj(v))
+                relax(e);
+        }
+    }
 
     private void relax(DirectedEdge e)
     {}
