294cab0daf8e260feb6756fffe4cc197fb1ce6ab
Jacob Stewart <jacobstewart1987@gmail.com>
2019-10-20 19:12:45 -0700

    constructor

--- Algs2/wk2/BellmanFordSP.java
+++ Algs2/wk2/BellmanFordSP.java
@@ -1,7 +1,29 @@
 public class BellmanFordSP
 {
+    private double[] distTo;
+    private DirectedEdge[] edgeTo;
+    private boolean[] onQ;
+    private Queue<Integer> queue;
+    private int cost;
+    private Iterable<DirectedEdge> cycle;
+
     public BellmanFordSP(EdgeWeightedDigraph G, int s)
     {
+        distTo = new Double[G.V()];
+        edgeTo = new DirectedEdge[G.V()];
+        onQ = new boolean[G.V()];
+        queue = new Queue<Integer>();
+        for (int v = 0; v < G.V(); v++)
+            distTo[v] = Double.POSTITIVE_INFINITY;
+        distTo[s] = 0.0;
+        queue.enqueue(s);
+        onQ[s] = true;
+        while (!queue.isEmpty() && !this.hasNegativeCycle())
+        {
+            int v = queue.dequeue();
+            onQ[v] = false;
+            relax(v);
+        }
     }
 
     private void relax(int v)
